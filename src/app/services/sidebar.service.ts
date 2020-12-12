1a627c6c2289732b233755436d9721afb7867d35
Daniel C Rubiano <dcrubiano01@gmail.com>
2020-12-12 17:44:45 -0500

    Fin seccion cambiando css de forma dinámica

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  public menu: any[] = [
+    {
+      titulo: 'Dashboard',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        {titulo: 'Main', url: '/'},
+        {titulo: 'ProgressBar', url: 'progress'},
+        // la ruta realmente es /dashboard/chart pero al ponerla sin / 
+        // la agrega a la ruta actual que en este casi es dashboard
+        {titulo: 'Gráficas', url: 'chart'},
+      ]
+    },
+
+  ];
+
+  constructor() { }
+}
